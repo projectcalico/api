c1bcd85ca9b56e168412ddab47d2260b997e0b4b
Daniel Fox <dan.fox@tigera.io>
2026-01-23 10:29:18 -0800

    Automatic API update from projectcalico/calico v3.30.6

--- pkg/apis/projectcalico/v3/felixconfig.go
+++ pkg/apis/projectcalico/v3/felixconfig.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2025 Tigera, Inc. All rights reserved.
+// Copyright (c) 2017-2026 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -301,8 +301,29 @@ type FelixConfigurationSpec struct {
 	// +kubebuilder:validation:Pattern=`^(?i)(Drop|Reject)?$`
 	IptablesFilterDenyAction string `json:"iptablesFilterDenyAction,omitempty" validate:"omitempty,dropReject"`
 
-	// LogPrefix is the log prefix that Felix uses when rendering LOG rules. [Default: calico-packet]
+	// LogPrefix is the log prefix that Felix uses when rendering LOG rules. It is possible to use the following specifiers
+	// to include extra information in the log prefix.
+	// - %t: Tier name.
+	// - %k: Kind (short names).
+	// - %n: Policy or profile name.
+	// - %p: Policy or profile name (namespace/name for namespaced kinds or just name for non namespaced kinds).
+	// Calico includes ": " characters at the end of the generated log prefix.
+	// Note that iptables shows up to 29 characters for the log prefix and nftables up to 127 characters. Extra characters are truncated.
+	// [Default: calico-packet]
+	// +kubebuilder:validation:Pattern=`^([a-zA-Z0-9%: /_-])*$`
 	LogPrefix string `json:"logPrefix,omitempty"`
+
+	// LogActionRateLimit sets the rate of hitting a Log action. The value must be in the format "N/unit",
+	// where N is a number and unit is one of: second, minute, hour, or day. For example: "10/second" or "100/hour".
+	// +optional
+	// +kubebuilder:validation:Pattern=`^[1-9]\d{0,3}/(?:second|minute|hour|day)$`
+	LogActionRateLimit *string `json:"logActionRateLimit,omitempty"`
+
+	// LogActionRateLimitBurst sets the rate limit burst of hitting a Log action when LogActionRateLimit is enabled.
+	// +optional
+	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:validation:Maximum=9999
+	LogActionRateLimitBurst *int `json:"logActionRateLimitBurst,omitempty"`
 
 	// LogFilePath is the full path to the Felix log. Set to none to disable file logging. [Default: /var/log/calico/felix.log]
 	LogFilePath string `json:"logFilePath,omitempty"`
